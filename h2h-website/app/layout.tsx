289be1f6f7c80817e1891b438bf4deb39ddf3101
Chitraksh <chitrakshsingh007@gmail.com>
2026-03-10 15:55:55 +0530

    bug fix

--- h2h-website/app/layout.tsx
+++ h2h-website/app/layout.tsx
@@ -1,4 +1,3 @@
-import { Metadata } from 'next'
 import './globals.css'
 
 export const metadata = {
@@ -17,12 +16,12 @@ export const metadata = {
   },
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="scroll-smooth">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9..144,400;0,9..144,600;0,9..144,700;0,9..144,900;1,9..144,400&family=Plus+Jakarta+Sans:wght@300;400;500;600;700&family=Space+Mono:wght@400;700&display=swap"
           rel="stylesheet"
